444791dc34f3020c9b1faa59531f8abfab8a8bc2
devmaroba <devmaroba@gmail.com>
2024-05-06 21:05:58 -0300

    Operadores_de_comparação

--- funções_de_entrada_e_saída.py
+++ funções_de_entrada_e_saída.py
@@ -2,4 +2,3 @@
 nome = input('Informe seu nome:')
 idade = input('Informe sua idade:')
 print('Você se chama', nome, 'e tem', idade, 'anos', end='\n')
-
---
+++ operadores_de_comparação.py
@@ -0,0 +1,10 @@
+
+saldo = 1000
+saque = 750
+
+print(saldo == saque)
+print(saldo != saque)
+print(saldo > saque)
+print(saldo - saque)
+print(saldo - saque * 2)
+print((saldo - saque) *2)
