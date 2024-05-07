4e978c41b8b4d614a726fc1a4c60080276ad0d47
devmaroba <devmaroba@gmail.com>
2024-05-06 22:07:48 -0300

    operadores_de_atribuição

---
+++ operadores_de_atribuição.py
@@ -0,0 +1,20 @@
+saldo = 500
+print(saldo)
+
+saldo += 300
+print(saldo)
+
+saldo -= 50
+print(saldo)
+
+saldo *= 2
+print(saldo)
+
+saldo /= 2
+print(saldo)
+
+saldo //= 2
+print(saldo)
+
+saldo %= 47
+print(saldo)
